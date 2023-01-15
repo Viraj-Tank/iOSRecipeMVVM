16f4fa848be6e13513c76a485cd5598acb1e54d1
VirajTank <virajtank98@gmail.com>
2023-01-15 11:31:18 +0530

    Initial Commit

---
+++ iOSRecipeMVVM/iOSRecipeMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSRecipeMVVMApp.swift
+//  iOSRecipeMVVM
+//
+//  Created by Viraj Tank on 15/01/23.
+//
+
+import SwiftUI
+
+@main
+struct iOSRecipeMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
